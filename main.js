868e86fc90b9843adfa7b3d6529bafa95865bea8
Miguel <migue0mpx@gmail.com>
2018-10-03 15:02:13 -0500

    Menu del programa.

--- main.js
+++ main.js
@@ -1,39 +1,123 @@
-const {app, BrowserWindow} = require('electron');
+const {app, BrowserWindow, Menu, dialog} = require('electron');
 const {TILES, TILEALT, TILELONG, COLORES, MAZE} = require('./misc/tiles');
 const {leerArchivo} = require('./misc/mapa');
 
-let file = 'mapa.txt';
+let file = 'maze.txt';
 global.mapa = leerArchivo(file);
 global.colores = COLORES;
 global.tilealt = TILEALT;
 global.tilelong = TILELONG;
 global.maze = MAZE;
-global.tipo = 'terrainmap';
+global.tipo = 'maze';
 global.initialPos = {x: 0, y:0};
 global.endPos = {x:0, y:0};
 global.being = {name: ''};
 
 
 let win;
+let child;
+let child2;
 
-function createWindow () {
-    win = new BrowserWindow({width: 900, height: 425, resizable: false, show: false});
-    win.loadFile('terrainmap.html');
-    let child = new BrowserWindow({width: 450, height: 221, parent: win, modal: true, show: false});
-    let child2 = new BrowserWindow({width: 450, height: 221, parent: win, modal: true, show: false});
+const template = [
+    {
+        label: 'Map',
+        submenu: [
+            {
+                label: 'Maze',
+                click(){
+                    if(global.tipo !== 'maze'){
+                        global.mapa = leerArchivo('maze.txt');
+                        global.tipo = 'maze';
+                        win.loadFile('maze.html');
+                        instanceChild();
+                        child.show();
+                        child.on('close', () => {
+                           win.reload();
+                        });
+                    }
+                }
+            },
+            {
+                label: 'TerrainMap',
+                click(){
+                    if(global.tipo !== 'terrainmap'){
+                        global.mapa = leerArchivo('mapa.txt');
+                        global.tipo = 'terrainmap';
+                        win.loadFile('terrainmap.html');
+                        instanceChild();
+                        instanceChild2();
+                        child.show();
+                        child.on('close', () => {
+                            child2.show();
+                        });
+                        child2.on('close', () => {
+                            win.reload();
+                        });
+                    }
+                }
+            }
+
+        ]
+    },
+    {
+        label: 'Load file',
+        click(){
+            console.log(dialog.showOpenDialog({properties: ['openFile']}));
+            global.mapa = leerArchivo(dialog.showOpenDialog({properties: ['openFile']})[0]);
+            instanceChild();
+            instanceChild2();
+            child.show();
+            child.on('close', () => {
+                if(global.tipo === 'terrainmap'){
+                    child2.show();
+                }
+                else{
+                    win.reload();
+                }
+            });
+
+        }
+    },
+    {
+        label: 'Reload',
+        click(){
+            win.reload();
+        }
+    },
+    {
+        label: 'Console',
+        role: 'toggleDevTools'
+    }
+
+];
+
+function instanceChild(){
+    child = new BrowserWindow({width: 450, height: 221, parent: win, modal: true, show: false});
     child.loadFile('dialogs/posInicial.html');
     child.setAutoHideMenuBar(true);
     child.setMenuBarVisibility(false);
+}
+function instanceChild2() {
+    child2 = new BrowserWindow({width: 450, height: 221, parent: win, modal: true, show: false});
+    child2.loadFile('dialogs/seleccionarSer.html');
+    child2.setMenuBarVisibility(false);
+}
+
+function createWindow () {
+    win = new BrowserWindow({width: 900, height: 425, resizable: false, show: false});
+    win.loadFile('maze.html');
+    instanceChild();
+    instanceChild2();
 
     win.once('ready-to-show', () => {
+        Menu.setApplicationMenu(Menu.buildFromTemplate(template));
         win.show();
     });
-    child.once('ready-to-show', () => {
+    child.on('ready-to-show', () => {
         child.show();
     });
     child.on('closed', () => {
         if(global.tipo === 'terrainmap'){
-            child2.loadFile('dialogs/seleccionarSer.html');
             child2.show();
         }
         else{
@@ -65,6 +149,3 @@ app.on('activate', () => {
         createWindow()
     }
 });
-
-
-
