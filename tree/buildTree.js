46f6e9a923a5b6c0bf4a6275b87a1c81d0f0c00c
Miguel E. Rodríguez <migue0mpx@gmail.com>
2019-04-03 14:09:26 -0600

    Se ha integrado el modulo para graficar el arbol.

--- tree/buildTree.js
+++ tree/buildTree.js
@@ -8,7 +8,6 @@ let settings = {
 
 let nodes = [];
 
-
 for (let node of gTree.nodes){
     let newNode = {};
     if(nodes.length === 0){
@@ -22,56 +21,11 @@ for (let node of gTree.nodes){
 }
 
 
-let node0 = {
-    text: {name: gTree.nodes[0].coords}
-};
-let node1 = {
-    text: {name: gTree.nodes[1].coords},
-    parent: node0
-};
-
-let node2 = {
-    text: {name: gTree.nodes[2].coords},
-    parent: node1
-};
-
-let node3 = {
-    text: {name: gTree.nodes[3].coords},
-    parent: node1
-};
-
-let nodesT = [node0, node1, node2, node3];
-
-
 let config = [
     settings
 ];
 
 config = config.concat(nodes);
 
-// let config = {
-//     chart: {
-//         container: '#tree-simple',
-//         connectors: {type: 'step', style: {
-//
-//             }}
-//     },
-//     nodeStructure: {
-//         text: {name: gTree.nodes[0].coords},
-//         children: [
-//             {
-//                 text: {name: gTree.nodes[1].coords},
-//                 children: [
-//                     {
-//                         text: {name: gTree.nodes[2].coords}
-//                     },
-//                     {
-//                         text: {name: gTree.nodes[3].coords}
-//                     }
-//                 ]
-//             }
-//         ]
-//     }
-// };
 
 let my_chart = new Treant(config);
